84f346bb03151d7ea174c69932f21d388bc4833c
4020a6 <4020a6@chironja.ece.uprm.edu>
2018-03-12 11:20:17 -0400

    Staring third exercise

--- LinkedListLab/src/linkedLists/SLFLList.java
+++ LinkedListLab/src/linkedLists/SLFLList.java
@@ -14,21 +14,39 @@ import linkedLists.LinkedList;
 public class SLFLList<E> extends SLList<E>
 {
 	private SNode<E> first, last;   // reference to the first node and to the last node
+	private SNode<E> current;
 	int length; 
 	
 	public SLFLList() {       // to create an empty list instance
-		first = last = null; 
+		first = last = current = null; 
 		length = 0; 
 	}
 	
 	
 	public void addFirstNode(Node<E> nuevo) {
 		// TODO Auto-generated method stub
-		
+		if(length == 0){
+			first = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+			last = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+		}
+		else if (length == 1){			
+			first = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+			first.setNext(last);
+		}
+		else{
+			current = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+			current.setNext(first);
+			first = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+		}
+		length++;
 	}
 
 	public void addNodeAfter(Node<E> target, Node<E> nuevo) {
 		// TODO Auto-generated method stub
+		if(target.equals(last)){
+			last.setNext((linkedLists.AbstractSLList.SNode<E>) nuevo);
+			last = (linkedLists.AbstractSLList.SNode<E>) nuevo;
+		}
 		
 	}
 
@@ -38,29 +56,36 @@ public class SLFLList<E> extends SLList<E>
 	}
 
 	public Node<E> getFirstNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		return first;
 	}
 
 	public Node<E> getLastNode() throws NoSuchElementException {
-		// TODO Auto-generated method stub
-		return null;
+		return last;
 	}
 
 	public Node<E> getNodeAfter(Node<E> target) throws NoSuchElementException {
 		// TODO Auto-generated method stub
+		if(target.equals(last)){
+			throw new NoSuchElementException();
+		}
+		else if(target.equals(first)){
+			return first.getNext();
+		}
 		return null;
 	}
 
 	public Node<E> getNodeBefore(Node<E> target)
 			throws NoSuchElementException {
 		// TODO Auto-generated method stub
+		if(target.equals(first)){
+			throw new NoSuchElementException();
+		}
+		
 		return null;
 	}
 
 	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
+		return length;
 	}
 
 	public void removeNode(Node<E> target) {
